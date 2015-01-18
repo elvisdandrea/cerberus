686034c710282ec4304e090494b35e0f5b7c26a1
Elvis DAndrea <elvis.vista@gmail.com>
2015-01-18 14:50:56 -0200

    Fatal error handler

--- handler.php
+++ handler.php
@@ -23,21 +23,53 @@ error_reporting(E_ERROR | E_PARSE);
 register_shutdown_function('fatalErrorHandler');
 spl_autoload_register('autoLoad');
 
+/**
+ * Is it an ajax request?
+ *
+ * A function outside core to handle
+ * when even core isn't working
+ *
+ * @return  bool
+ */
+function isAjax() {
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
+/**
+ * Is it running localhost server or prod server?
+ *
+ * @return bool
+ */
+function isLocal() {
+    return (strpos($_SERVER['SERVER_ADDR'], '192.168') !== false || $_SERVER['HTTP_HOST'] == 'localhost');
+}
+
 
 /**
  * Fatal Error Handler
+ *
+ * To support all kind of errors, this must
+ * be text-based instead of loading template files
  */
 function fatalErrorHandler(){
     $error = error_get_last();
-    if ($error['type'] === E_ERROR || $error['type'] === E_USER_ERROR || $error['type'] === E_PARSE || $error['type'] === E_COMPILE_ERROR) {
-        $var = '<h4>Tipo Erro: <b>' . $error['type'] . "</b><h4>";
-        $var .= '<h4><b>Mensagem</b>: ' . $error['message'] . "<h4>";
-        $var .= '<h3><b>Arquivo:</b> ' . $error['file'] . "</h3>";
-        $var .= '<h3><b>Linha:</b> ' . $error['line'] . "</h3>";
-
-        echo $var;
-        exit;
-    }
+    if (!in_array($error['type'],
+        array(
+            E_ERROR,
+            E_USER_ERROR,
+            E_PARSE,
+            E_COMPILE_ERROR
+        ))
+    ) return; ?>
+    <h2>Sorry, something went bad!</h2>
+    I know this is emarassing, but the server must be under maintenance. Please come back later.
+    <?php if (ENVDEV == '0') exit; ?>
+    <br><br>
+    Error: <?php echo $error['type']; ?> <br>
+    Message: <?php echo $error['message']; ?> <br>
+    File: <?php echo $error['file']; ?> <br>
+    Line: <?php echo $error['line']; ?>
+    <?php exit;
 }
 
 /**
